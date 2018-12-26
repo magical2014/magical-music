affbfd75f4a7a5a043fd4e6c348eb473c4ee4a92
zcx <732597785@qq.com>
2018-12-26 17:24:36 +0800

    add LocalPlayListActivity, no function

---
+++ app/src/main/java/com/magical/music/localmusic/LocalPlayListActivity.java
@@ -0,0 +1,13 @@
+package com.magical.music.localmusic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocalPlayListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_play_list);
+    }
+}
